089cae8ef1b2ecd21f7eb546264ea77fd62028c6
Shiva <102786796+SUNDI6069@users.noreply.github.com>
2022-12-11 21:45:26 +0530

    Add files via upload

---
+++ insta-clone/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import './App.css';
+import Header from './components/header/header';
+import Postview from './components/PostView/postview';
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Postview/>
+    </div>
+  );
+}
+
+export default App;
